4bb891dd4e43ecd21dd326b7c2d0f6542f34daa9
tanxu.777 <tanxu.777@bytedance.com>
2022-04-03 15:16:06 +0800

    commit

--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
 
 function App() {
+  const [data, setData] = useState('nodata');
+  useEffect(() => {
+    fetch('https://restless-water-66af.18602863283.workers.dev/',{mode: 'cors'})
+  .then(response => response.json())
+  .then(data => setData(data));
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {JSON.stringify(data, null , 2)}
     </div>
   );
 }
